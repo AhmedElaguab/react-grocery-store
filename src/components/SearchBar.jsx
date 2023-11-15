dcb77144508216b5364042c562b117f6e5d8ab03
Ahmed Elaguab <elaguabahmed97@gmail.com>
2023-11-16 00:36:49 +0100

    Add search bar styles

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ export default function SearchBar({
     <form>
       <div>
         <input
-          className="search"
+          className="w-full border-2 rounded-md text-lg p-2 mb-2 border-gray-200 focus:border-gray-800"
           type="text"
           placeholder="Search..."
           value={filterText}
@@ -19,16 +19,21 @@ export default function SearchBar({
         />
       </div>
       <div>
-        <input
-          type="checkbox"
-          name="in-stock-only"
-          id="in-stock-only"
-          checked={inStockOnly}
-          onChange={() => {
-            onInStockOnlyChange(!inStockOnly)
-          }}
-        />
-        <label htmlFor="in-stock-only">Only show products on stock</label>
+        <label htmlFor="in-stock-only" className="flex">
+          <input
+            className="absolute h-4 w-4  accent-gray-700 bg-grey-700 text-red-500 rounded cursor-pointer"
+            type="checkbox"
+            name="in-stock-only"
+            id="in-stock-only"
+            checked={inStockOnly}
+            onChange={() => {
+              onInStockOnlyChange(!inStockOnly)
+            }}
+          />
+          <span className="text-sm ml-6 uppercase font-medium">
+            Only show products on stock
+          </span>
+        </label>
       </div>
     </form>
   )
