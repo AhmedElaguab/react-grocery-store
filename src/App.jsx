dd0e37dcec4ff91282837e976ff3940617bd2d10
Ahmed Elaguab <elaguabahmed97@gmail.com>
2023-11-15 19:26:14 +0100

    Break UI into multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,70 @@
 import './styles.css'
 
+function SearchBar() {
+  return (
+    <div>
+      <div>
+        <input className="search" type="text" placeholder="Search..." />
+      </div>
+      <div>
+        <input type="checkbox" name="only-in-stock" id="only-in-stock" />
+        <label htmlFor="only-in-stock">Only show products on stock</label>
+      </div>
+    </div>
+  )
+}
+
+function ProductTableCategory({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
+  )
+}
+
+function ProductTableRow() {
+  return (
+    <>
+      <tr>
+        <td>Apple </td>
+        <td>$1</td>
+      </tr>
+      <tr>
+        <td>Dragonfruit </td>
+        <td>$1</td>
+      </tr>
+      <tr>
+        <td>Passionfruit </td>
+        <td>$2</td>
+      </tr>
+    </>
+  )
+}
+
+function ProductTable() {
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Name </th>
+            <th>Price</th>
+          </tr>
+          <ProductTableCategory category="Fruits" />
+          <ProductTableRow />
+          <ProductTableCategory category="Vegetables" />
+          <ProductTableRow />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
 export default function App() {
   return (
     <div className="App">
-      <div>
-        <div>
-          <input className="search" type="text" placeholder="Search..." />
-        </div>
-        <div>
-          <input type="checkbox" name="only-in-stock" id="only-in-stock" />
-          <label htmlFor="only-in-stock">Only show products on stock</label>
-        </div>
-      </div>
-      <div>
-        <table>
-          <tbody>
-            <tr>
-              <th>Name </th>
-              <th>Price</th>
-            </tr>
-            <tr>
-              <th colspan="2">Fruits</th>
-            </tr>
-            <tr>
-              <td>Apple </td>
-              <td>$1</td>
-            </tr>
-            <tr>
-              <td>Dragonfruit </td>
-              <td>$1</td>
-            </tr>
-            <tr>
-              <td>Passionfruit </td>
-              <td>$2</td>
-            </tr>
-            <tr>
-              <th colspan="2">Vegetables</th>
-            </tr>
-            <tr>
-              <td>Spinach </td>
-              <td>$2</td>
-            </tr>
-            <tr>
-              <td>Pumpkin </td>
-              <td>$4</td>
-            </tr>
-            <tr>
-              <td>Peas </td>
-              <td>$1</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <SearchBar />
+      <ProductTable />
     </div>
   )
 }
